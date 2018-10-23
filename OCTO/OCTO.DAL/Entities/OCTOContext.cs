296f76ac7b82f26cf542da5dd5a37e96f6f95f1b
Narek Yegoryan <narek.yegoryan@alenasolutions.com>
2018-10-23 21:39:13 +0400

    Remove unused parts of code

--- OCTO/OCTO.DAL/Entities/OCTOContext.cs
+++ OCTO/OCTO.DAL/Entities/OCTOContext.cs
@@ -8,10 +8,9 @@ namespace OCTO.DAL.Entities
 
         public DbSet<Contact> Contacts { get; set; }
 
-        protected override async void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Salutation>().HasIndex(x => x.Name).HasName("IX_Salutation_Name").IsUnique();
-            var t = await this.Database.BeginTransactionAsync();
         }
     }
 }
