38d62d063afe3e381c05184ebca3c69f202ff377
narekye <yegoryan.narek@gmail.com>
2018-12-11 11:34:25 +0400

    Account filter corrections

--- OCTO/OCTO.BLL.Models/Filters/AccountFilterModel.cs
+++ OCTO/OCTO.BLL.Models/Filters/AccountFilterModel.cs
@@ -2,6 +2,8 @@
 {
     public class AccountFilterModel
     {
-        
+        public int Id { get; set; }
+        public string Zip { get; set; }
+        public string Name { get; set; }
     }
 }
